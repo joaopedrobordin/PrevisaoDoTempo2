42dbaabbc98db0b6bbd3d7ff61195c3d709463a2
joaopedrobordin <bpj7@hotmail.com>
2022-07-21 21:37:12 -0300

    PTyoutube
    copiado do youtube

---
+++ appPDT/www/js/index.js
@@ -0,0 +1,17 @@
+const api = {
+    key:
+    base:
+    lang:
+    units: 
+}
+
+const city = document.querySelectort('.city')
+const date = document.querySelector('.date');
+const container_img = document.querySelector('.container-img');
+const container_temp = document.querySelector('.container-temp');
+const temp_number = document.querySelector('.container-temp div');
+const temp_unit = document.querySelector('.container-temp span');
+const weather_t = document.querySelector('.weather');
+const search_input = document.querySelector('.form-controle')
+const search_button = document.querySelector('.btn');
+const low_high = document.querySelector.('.low-high');
